cc1d5653490cf940ab62c15af391fb1ce6826791
rren8957 <96750880+rren8957@users.noreply.github.com>
2022-02-11 15:21:37 -0500

    Code for react-task-tracker

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import { useState } from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+
+
+
+const App = () => {
+
+  const [tasks, setTasks] = useState([ 
+{
+  id: 1,
+  text: 'Doctors Appointment',
+  day: 'Feb 5th at 2:30pm',
+  reminder: true,
+},
+{
+  id: 2,
+  text: 'Meeting at School',
+  day: 'Feb 6th at 1:30 pm',
+  reminder: true,
+},
+{
+  id: 3,
+  text: 'Food Shopping',
+  day: 'Feb 5th at 2:30 pm',
+  reminder: false,
+},
+
+])
+
+// Delete Task
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task) => task.id !== id))
+}
+
+// Toggle Reminder
+const toggleReminder = (id) => {
+  console.log(id)
+}
+
+  return (
+    <div className="container">
+      <Header />
+      {tasks.length > 0 ? 
+        <Tasks tasks={tasks} onDelete={deleteTask}
+        onToggle={toggleReminder}/>
+        : 'No Tasks To Show'}
+    </div>
+  )
+}
+
+/* example using a class */
+// class App extends React.Component {
+//   render() {
+//     return <Header />
+//   }
+// }
+
+export default App;
